3369572800f882a52aeccac6dcb3444e6c5cab38
Stefan Zabka <stefan.zabka@camunda.com>
2024-02-12 12:47:47 +0100

    fix(terraform): enable remote backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,12 +2,12 @@ locals {
   bucket_name = "${var.google_project_id}-tfstate"
 }
 
-# terraform {
-#   backend "gcs" {
-#     bucket = "hapdfy-tfstate"
-#     prefix = "terraform/state"
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "hapdfy-tfstate"
+    prefix = "terraform/state"
+  }
+}
 resource "google_storage_bucket" "tf_state" {
   name          = local.bucket_name
   force_destroy = false
